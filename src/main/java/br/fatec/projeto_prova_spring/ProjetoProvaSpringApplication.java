1d1137629cf1cfff316dea4db8e34aa8b84ec6eb
Nicolas Pereira <nicolas.bonifa@gmail.com>
2024-09-30 17:04:36 -0300

    dsdsd

---
+++ src/main/java/br/fatec/projeto_prova_spring/ProjetoProvaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.fatec.projeto_prova_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoProvaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoProvaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fatec/projeto_prova_spring/ProjetoProvaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fatec.projeto_prova_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoProvaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
